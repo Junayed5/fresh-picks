7011d36edd92a3b467f32b489827f5f1d767a9ee
Junayed5 <mdjunayed352003@gmail.com>
2024-08-29 09:33:23 +0600

    create trending section

---
+++ app/components/homeItems/Trending.js
@@ -0,0 +1,71 @@
+import React from "react";
+
+const Trending = () => {
+  const products = [
+    {
+      name: "Hento Deals Security Tire Grid",
+      image: "lip.jpg",
+      mainPrice: 100.0,
+      discountPrice: 80.0,
+    },
+    {
+      name: "Hento Deals Security Tire Grid",
+      image: "egg.jpg",
+      mainPrice: 100.0,
+      discountPrice: 80.0,
+    },
+    {
+      name: "Hento Deals Security Tire Grid",
+      image: "orange.jpg",
+      mainPrice: 100.0,
+      discountPrice: 80.0,
+    },
+    {
+      name: "Hento Deals Security Tire Grid",
+      image: "https://demo.xpeedstudio.com/marketov2/grocery/wp-content/uploads/sites/12/2018/10/13-min.jpg",
+      mainPrice: 100.0,
+      discountPrice: 80.0,
+    },
+  ];
+  return (
+    <div className="max-w-7xl mx-auto">
+      <div className="grid grid-cols-1 md:grid-cols-4 py-20">
+        {products.map(({ name, image, discountPrice, mainPrice }) => (
+          <div className="border w-fit px-7 py-3">
+            <img className="w-48 mx-auto mb-7" src={image} alt="" />
+            <p className="font-semibold w-48 mb-1">{name}</p>
+            <div className="flex gap-x-5 text-base font-semibold">
+              <p className="text-gray-400 line-through">${mainPrice}</p>
+              <p className="text-primaryLite">${discountPrice}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <div className="h-24 shadow-md flex gap-x-20  justify-center  items-center">
+        <div className="border-r px-8">
+            <p>Free Delivery</p>
+            <p>From $78</p>
+        </div>
+        <div className="border-r px-8">
+            <p>Free Delivery</p>
+            <p>From $78</p>
+        </div>
+        <div className="border-r px-8">
+            <p>Free Delivery</p>
+            <p>From $78</p>
+        </div>
+        <div className="border-r px-8">
+            <p>Free Delivery</p>
+            <p>From $78</p>
+        </div>
+        <div className="px-8">
+            <p>Free Delivery</p>
+            <p>From $78</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Trending;
--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 import Navbar from "./components/Navbar";
-import ProductCategory from "./components/ProductCategory";
+import ProductCategory from "./components/homeItems/ProductCategory";
+import Trending from "./components/homeItems/Trending";
 import Slider from "./components/slider/Slider";
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
     <main>
       <Slider/>
       <ProductCategory/>
+      <Trending/>
     </main>
   );
 }
