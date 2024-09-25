1314e0e2f4230f18bf7fdc700ec6c0494a369fa0
Junayed5 <mdjunayed352003@gmail.com>
2024-09-25 23:12:06 +0600

    Login page working

--- app/login/page.js
+++ app/login/page.js
@@ -1,27 +1,67 @@
 import React from "react";
+import { FaFacebookF } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
 
 const page = () => {
   return (
-    <div className="bg-[url('https://demo.xpeedstudio.com/marketov2/grocery/wp-content/uploads/sites/12/revslider/grocery/banner3_img-min-1.jpg')] bg-no-repeat bg-center bg-cover h-[600px]">
-      <div className="max-w-7xl mx-auto">
-        <div className="bg-white h-96 rounded-lg">
-          <form action="">
+    <div className="bg-[url('https://demo.xpeedstudio.com/marketov2/grocery/wp-content/uploads/sites/12/revslider/grocery/banner3_img-min-1.jpg')] bg-no-repeat bg-center bg-cover h-screen">
+      <div className="max-w-7xl mx-auto pt-48">
+        <form
+          className="bg-white h-96 w-3/5 rounded-lg grid grid-cols-2   px-10 py-14"
+          action=""
+        >
+          <div className="border-r">
             <div>
-              <label htmlFor="Email">Email</label><br />
-              <input className="h-10 w-80 rounded-full bg-gray-100 pl-5 py-6 my-2" placeholder="Enter Your Email" type="email" name="email" id="email  " />
+              <label htmlFor="Email">Email</label>
+              <br />
+              <input
+                className="h-10 w-80 rounded-full bg-gray-100 pl-5 py-6 my-2"
+                placeholder="Enter Your Email"
+                type="email"
+                name="email"
+                id="email  "
+              />
             </div>
             <div>
-              <label htmlFor="Password">Password</label><br />
-              <input className="h-10 w-80 rounded-full bg-gray-100 pl-5 py-6 my-2" placeholder="Enter Your Password" type="password" name="password" id="password  " />
+              <label htmlFor="Password">Password</label>
+              <br />
+              <input
+                className="h-10 w-80 rounded-full bg-gray-100 pl-5 py-6 my-2"
+                placeholder="Enter Your Password"
+                type="password"
+                name="password"
+                id="password  "
+              />
             </div>
-            <div>
-              <input type="checkbox" name="check" id="email  " />
+            <div className="my-2">
+              <input
+                className="bg-gray-100 border-none mr-2"
+                type="checkbox"
+                name="check"
+                id="email  "
+              />
               <label htmlFor="Remember Me">Remember Me</label>
             </div>
 
-            <input className="h-10 px-10 bg-primary hover:bg-primaryLite text-white rounded-full" type="submit" value="Login now" />
-          </form>
-        </div>
+            <input
+              className="h-10 px-10 bg-primary hover:bg-primaryLiten text-white rounded-full"
+              type="submit"
+              value="Login now"
+            />
+          </div>
+          <div className="flex justify-center items-center">
+            <div className="">
+              <button className="h-12 w-64 my-5 bg-gray-100 hover:bg-gray-200 rounded-full text-gray-800 flex justify-center items-center gap-2">
+                <FcGoogle />
+                continue With Goggle
+              </button>
+              <button className="h-12 w-64 my-5 bg-gray-100 hover:bg-gray-200 rounded-full text-gray-800 flex justify-center items-center gap-2">
+                <FaFacebookF />
+                continue With Facebook
+              </button>
+            </div>
+          </div>
+        </form>
       </div>
     </div>
   );
