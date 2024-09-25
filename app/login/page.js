099a0d2f1195d464f28895730347b36667022132
Junayed5 <mdjunayed352003@gmail.com>
2024-09-25 23:30:19 +0600

    login page done

--- app/login/page.js
+++ app/login/page.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 import { FaFacebookF } from "react-icons/fa";
 import { FcGoogle } from "react-icons/fc";
@@ -7,7 +8,7 @@ const page = () => {
     <div className="bg-[url('https://demo.xpeedstudio.com/marketov2/grocery/wp-content/uploads/sites/12/revslider/grocery/banner3_img-min-1.jpg')] bg-no-repeat bg-center bg-cover h-screen">
       <div className="max-w-7xl mx-auto pt-48">
         <form
-          className="bg-white h-96 w-3/5 rounded-lg grid grid-cols-2   px-10 py-14"
+          className="bg-white h-96 w-3/5 mx-auto rounded-lg grid grid-cols-2   px-10 py-14"
           action=""
         >
           <div className="border-r">
@@ -15,7 +16,7 @@ const page = () => {
               <label htmlFor="Email">Email</label>
               <br />
               <input
-                className="h-10 w-80 rounded-full bg-gray-100 pl-5 py-6 my-2"
+                className="h-10 w-80 rounded-full bg-gray-100 pl-5 py-6 my-2 "
                 placeholder="Enter Your Email"
                 type="email"
                 name="email"
@@ -50,15 +51,25 @@ const page = () => {
             />
           </div>
           <div className="flex justify-center items-center">
-            <div className="">
-              <button className="h-12 w-64 my-5 bg-gray-100 hover:bg-gray-200 rounded-full text-gray-800 flex justify-center items-center gap-2">
-                <FcGoogle />
-                continue With Goggle
-              </button>
-              <button className="h-12 w-64 my-5 bg-gray-100 hover:bg-gray-200 rounded-full text-gray-800 flex justify-center items-center gap-2">
-                <FaFacebookF />
-                continue With Facebook
-              </button>
+            <div>
+              <div>
+                <p className="text-center text-sm font-serif">Or <br /> Login with</p>
+              </div>
+              <div className="">
+                <button className="h-12 w-64 my-5 text-sm font-semibold bg-gray-100 hover:bg-gray-200 rounded-full text-gray-800 flex justify-center items-center gap-2">
+                  <FcGoogle />
+                  continue With Goggle
+                </button>
+                <button className="h-12 w-64 my-5 text-sm font-semibold bg-gray-100 hover:bg-gray-200 rounded-full text-gray-800 flex justify-center items-center gap-2">
+                  <FaFacebookF />
+                  continue With Facebook
+                </button>
+              </div>
+              <div>
+                <p className="text-sm text-center">
+                  Not have any account? <Link className="text-primary" href={'/signUp'}>SignUp</Link> here
+                </p>
+              </div>
             </div>
           </div>
         </form>
---
+++ app/signUp/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            sign up here
+        </div>
+    );
+};
+
+export default page;
